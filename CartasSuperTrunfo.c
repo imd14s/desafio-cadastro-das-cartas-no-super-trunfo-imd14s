165d7d1cfe60e4ffa09140d83ec9ea87a1598799
imd14s <eversond14s@outlook.com>
2025-04-29 12:28:31 -0300

    added comments

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,10 +23,13 @@ void add_attributes(Card *new_card) {
 
     printf("Digite a quantidade de pontos turisticos: \n");
     scanf("%d", &new_card->tourist_spot_numbers);
+    
+    // adicionando o calculo para a struct
     new_card->population_density = new_card->population / new_card->field;
     new_card->pib_capital = new_card->pib / new_card->population;
 }
 
+// função responsavel de exibir os atributos.
 void viwer(int play, Card *card) {
     printf("carta %d \n \n", play);
     printf("População: %2.f \n", card->population);
@@ -42,12 +45,22 @@ int main() {
     Card card_one;
     Card card_two;
 
+    // adicionando primeira carta
     add_attributes(&card_one);
+    
     printf(" \n \n adicionado carta 2 \n \n ");
+    
+    // adicionando segunda carta
     add_attributes(&card_two);
+    
     printf("\n \n Carta 1 \n \n");
+    
+    // exibindo primeira carta
     viwer(1, &card_one);
+    
     printf("\n \n Carta 2 \n \n");
+    
+    //exibindo segunda carta
     viwer(2, &card_two);
     
     return 0;
